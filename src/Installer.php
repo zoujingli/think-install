7554c3eddfb70d7eca1de4c88677e75a94781ebb
邹景立 <zoujingli@qq.com>
2025-01-06 23:50:45 +0800

    fix: 更新插件安装，如果是本地模式不复制及清理目录

--- src/Installer.php
+++ src/Installer.php
@@ -27,7 +27,7 @@ class Installer extends LibraryInstaller
     public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
     {
         return parent::install($repo, $package)->then(function () use ($package) {
-            $this->copyStaticFiles($package);
+            $this->installPlugin($package);
             if (($extra = $package->getExtra()) && !empty($extra['plugin']['event'])) {
                 $path = $this->getInstallPath($package);
                 foreach ((array)$extra['plugin']['event'] as $file => $class) {
@@ -68,7 +68,7 @@ class Installer extends LibraryInstaller
     {
         if (is_dir($this->getInstallPath($target))) {
             return parent::update($repo, $initial, $target)->then(function () use ($target) {
-                $this->copyStaticFiles($target);
+                $this->installPlugin($target);
             });
         } else {
             return $this->install($repo, $target);
@@ -96,70 +96,72 @@ class Installer extends LibraryInstaller
         return parent::getInstallPath($package);
     }
 
-    protected function copyStaticFiles(PackageInterface $package)
+    private function installPlugin(PackageInterface $package)
     {
-        if ($this->composer->getPackage()->getType() === 'project') {
-            $extra = $package->getExtra();
-            $installPath = $this->getInstallPath($package);
-            $this->io->write("\r  > Exec Plugin <info>{$package->getPrettyName()} </info> \033[K");
+        if ($this->composer->getPackage()->getType() !== 'project') {
+            return;
+        }
 
-            // 初始化，若文件存在不进行操作
-            if (!empty($extra['plugin']['init'])) {
-                foreach ((array)$extra['plugin']['init'] as $source => $target) {
-                    if (!is_file($target) && is_file($sfile = $installPath . DIRECTORY_SEPARATOR . $source)) {
-                        $this->io->write("\r  > Init Source <info>{$source} </info>to <info>{$target} </info>");
-                        file_exists(dirname($target)) || mkdir(dirname($target), 0755, true);
-                        $this->filesystem->copy($sfile, $target);
-                    }
-                }
-            }
+        $extra = $package->getExtra();
+        $installPath = $this->getInstallPath($package);
+        $this->io->write("\r  > Exec Plugin <info>{$package->getPrettyName()} </info> \033[K");
 
-            // 复制替换，无论是否存在都进行替换
-            if (!empty($extra['plugin']['copy'])) {
-                foreach ((array)$extra['plugin']['copy'] as $source => $target) {
-
-                    // 是否为绝对复制模式
-                    $force = $target[0] === '!' && file_exists($target = substr($target, 1));
-
-                    // 源文件位置，若不存在直接跳过
-                    if (!file_exists($sfile = $installPath . DIRECTORY_SEPARATOR . $source)) continue;
-
-                    // 检查目标文件，若已经存在直接跳过
-                    if (is_file($sfile) && is_file($target) && md5_file($sfile) === md5_file($target)) continue;
-
-                    // 如果目标目录或其上级目录下存在 ignore 文件则跳过复制
-                    if (file_exists(dirname($target) . '/ignore') || file_exists(rtrim($target, '\\/') . "/ignore")) {
-                        $this->io->write("\r  > Skip Copy <info>{$source} </info>to <info>{$target} </info>");
-                        continue;
-                    }
-
-                    // 绝对复制时需要先删再写入
-                    if (($action = $force && file_exists($target) ? 'Push' : 'Copy') === 'Push') {
-                        is_file($target) ? $this->filesystem->unlink($target) : $this->filesystem->removeDirectoryPhp($target);
-                    }
-
-                    // 执行复制操作，将原文件或目录复制到目标位置
-                    $this->io->write("\r  > {$action} Source <info>{$source} </info>to <info>{$target} </info>");
+        // 初始化，若文件存在不进行操作
+        if (!empty($extra['plugin']['init'])) {
+            foreach ((array)$extra['plugin']['init'] as $source => $target) {
+                if (!is_file($target) && is_file($sfile = $installPath . DIRECTORY_SEPARATOR . $source)) {
+                    $this->io->write("\r  > Init Source <info>{$source} </info>to <info>{$target} </info>");
                     file_exists(dirname($target)) || mkdir(dirname($target), 0755, true);
                     $this->filesystem->copy($sfile, $target);
                 }
             }
+        }
 
-            // 清理当前库的所有文件及信息
-            if (!empty($extra['plugin']['clear'])) {
-                $rootPath = dirname($this->vendorDir);
-                if (stripos($installPath, $rootPath) === 0) {
-                    try {
-                        $showPath = substr($installPath, strlen($rootPath) + 1);
-                        $this->io->write("\r  > Clear Vendor <info>{$showPath} </info>");
-                        $this->filesystem->removeDirectoryPhp($installPath);
-                    } catch (\Exception|\RuntimeException $exception) {
-                        $this->io->error("\r  > {$exception->getMessage()}");
-                    }
-                } else {
-                    $this->io->write("\r  > Skip Clear <info>{$installPath} </info>");
+        // 非 vendor 目录，不执行 copy 及 clear 操作，直接使用插件模式
+        if (stripos(realpath($installPath), $this->vendorDir) !== 0) {
+            $this->io->write(sprintf("\r  > Skip Copy and Clear <info>%s </info>", realpath($installPath)));
+            return;
+        }
+
+        // 复制替换，无论是否存在都进行替换
+        if (!empty($extra['plugin']['copy'])) {
+            foreach ((array)$extra['plugin']['copy'] as $source => $target) {
+
+                // 是否为绝对复制模式
+                $force = $target[0] === '!' && file_exists($target = substr($target, 1));
+
+                // 源文件位置，若不存在直接跳过
+                if (!file_exists($sfile = $installPath . DIRECTORY_SEPARATOR . $source)) continue;
+
+                // 检查目标文件，若已经存在直接跳过
+                if (is_file($sfile) && is_file($target) && md5_file($sfile) === md5_file($target)) continue;
+
+                // 如果目标目录或其上级目录下存在 ignore 文件则跳过复制
+                if (file_exists(dirname($target) . '/ignore') || file_exists(rtrim($target, '\\/') . "/ignore")) {
+                    $this->io->write("\r  > Skip Copy <info>{$source} </info>to <info>{$target} </info>");
+                    continue;
                 }
+
+                // 绝对复制时需要先删再写入
+                if (($action = $force && file_exists($target) ? 'Push' : 'Copy') === 'Push') {
+                    is_file($target) ? $this->filesystem->unlink($target) : $this->filesystem->removeDirectoryPhp($target);
+                }
+
+                // 执行复制操作，将原文件或目录复制到目标位置
+                $this->io->write("\r  > {$action} Source <info>{$source} </info>to <info>{$target} </info>");
+                file_exists(dirname($target)) || mkdir(dirname($target), 0755, true);
+                $this->filesystem->copy($sfile, $target);
             }
+        }
+
+        // 清理当前库的所有文件及信息
+        if (!empty($extra['plugin']['clear'])) try {
+            $rootPath = dirname($this->vendorDir);
+            $showPath = substr($installPath, strlen($rootPath) + 1);
+            $this->io->write("\r  > Clear Vendor <info>{$showPath} </info>");
+            $this->filesystem->removeDirectoryPhp($installPath);
+        } catch (\Exception|\RuntimeException $exception) {
+            $this->io->error("\r  > {$exception->getMessage()}");
         }
     }
 
